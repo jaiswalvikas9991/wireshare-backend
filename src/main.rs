46621d0e5da5b55699076d4d445ed134f3ae889c
jaiswalvikas9991 <jaiswalvikas9991@gmail.com>
2024-09-08 14:18:01 +0530

    Version updates

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,10 @@ use serde::{Deserialize, Serialize};
 use shuttle_axum::ShuttleAxum;
 use tokio::sync::Mutex;
 
-const ALPHABET_AND_NUMBERS: [char; 36] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
+const ALPHABET_AND_NUMBERS: [char; 36] = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+];
 struct State {
     users: HashMap<String, SplitSink<WebSocket, Message>>,
 }
